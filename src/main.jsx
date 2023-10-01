1bcb82eb7c015e69f0d6f46e4165b53ef00fe70a
Camilo <camilojpg30@gmail.com>
2023-10-01 02:47:23 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -14,8 +14,6 @@ axios.defaults.baseURL = "http://localhost:3001"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <React.StrictMode>
         <App />
-    </React.StrictMode>
   </Provider>,
 )
