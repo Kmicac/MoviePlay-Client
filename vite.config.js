41bad3c5bdeb111d8f57dcc943615a6f3261b995
Camilo <camilojpg30@gmail.com>
2023-10-01 03:44:36 -0300

    new

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://kmicac.github.io/MoviePlay-Client/",
   plugins: [react()],
 })
